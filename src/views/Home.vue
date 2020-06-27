8268e99336264ec68b521d14d47d6d3420f0a420
Corvinus <markus.corvinus0827@gmail.com>
2020-06-28 02:20:11 +0800

    #6-breakpoints and visibility

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,8 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
-    <v-btn color="pink">click me</v-btn>    
-    <v-btn depressed color="pink">click me</v-btn>    
-    <v-btn text color="pink">click me</v-btn>  
-
-    <v-btn depressed class="pink white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>email me</span>
-    </v-btn>    
-
-    <v-btn depressed class="pink white--text" small>
-      <v-icon left>mdi-email</v-icon>
-      <span>email me</span>
-    </v-btn>    
-
-    <v-btn depressed dark color="purple" large>
-      <span>email me</span>
-      <v-icon right large>mdi-email</v-icon>
-    </v-btn>    
-
-    <v-btn depressed dark color="purple" large fab>
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>    
+    <v-btn class="hidden-md-and-down">click me</v-btn>
+    <v-btn class="d-md-none">click me2</v-btn>
   </div>
 </template>
 
