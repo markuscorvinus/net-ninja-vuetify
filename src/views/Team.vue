dd91108eb992181dd64a62b370a620b8cc57e64d
Corvinus <markus.corvinus0827@gmail.com>
2020-06-28 22:10:30 +0800

    #19-team cards

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,13 +1,51 @@
 <template>
     <div class="team">
         <h1 class="subheading grey--text">Team</h1>
-        <v-container class="my-5">Content</v-container>
+        <v-container class="my-5">
+            <v-row>
+                <v-col xs12 sm6 md4 lg3 
+                    v-for="(person,index) in team" 
+                    :key="index"
+                >
+                    <v-card class="text-center ma-3" color="blue lighten-2">
+                        <v-img class="pt-4">
+
+                        </v-img>
+                        <v-card-text>
+                            <div class="text-h6">{{ person.name }}</div>
+                            <div class="dark">{{ person.role }}</div>
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <v-row>
+                                <v-col justify-center>
+                                    <v-btn flat color="grey lighten-3">
+                                        <v-icon small left>mdi-message</v-icon>
+                                        <span>Message Me</span>
+                                    </v-btn>
+                                </v-col>
+                            </v-row>
+                        </v-card-actions>        
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
     </div>    
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            team: [
+                { name: 'The Net Ninja', role: 'Web developer' },
+                { name: 'Ryu', role: 'Graphic designer' },
+                { name: 'Chun Li', role: 'Web developer' },
+                { name: 'Gouken', role: 'Social media maverick' },
+                { name: 'Yoshi', role: 'Sales guru'}
+            ]
+        }
+    }
 }
 </script>
 
