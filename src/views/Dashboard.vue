8046d61997f3e6491c903fd1c957a3e6a93d2263
Corvinus <markus.corvinus0827@gmail.com>
2020-06-28 16:46:10 +0800

    #13-grid system part 1

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="dashboard">
       <h1 class="subheading grey--text">Dashboard</h1>
-      <v-container class="my-5">Content</v-container>
+      <v-container class="my-5">
+        <v-row>
+          <v-flex xs12 md6 >
+            <v-btn outlined block color="primary">Click Me</v-btn>
+          </v-flex>
+          <v-flex xs4 md2 >
+            <v-btn outlined block color="primary">Click Me1</v-btn>
+          </v-flex>
+          <v-flex xs4 md2 >
+            <v-btn outlined block color="primary">Click Me2</v-btn>
+          </v-flex>
+          <v-flex xs4 md2 >
+            <v-btn outlined block color="primary">Click Me3</v-btn>
+          </v-flex>
+        </v-row>
+
+        <v-row justify="center">
+          <v-flex xs4 md3>
+            <v-btn outlined block color="success">1</v-btn>
+          </v-flex>
+          <v-flex xs4 md3>
+            <v-btn outlined block color="success">2</v-btn>
+          </v-flex>
+        </v-row>
+      </v-container>
   </div>  
 </template>
 
