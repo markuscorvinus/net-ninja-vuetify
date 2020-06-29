30f0c17878d25b45c10bfdc128e252e5d43a9114
Corvinus <markus.corvinus0827@gmail.com>
2020-06-30 00:49:23 +0800

    #24-Forms(part1)

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -8,14 +8,33 @@
             </v-btn>
       </template>
       <v-card>
-          <v-card-title>Add new project</v-card-title>
+          <v-card-title primary-title class="grey">Add New project</v-card-title>
+          <v-card-text>
+            <v-form class="py-3">
+                <v-text-field prepend-icon="mdi-pencil" label="Title" v-model="title" color="green" />
+                <v-textarea prepend-icon="mdi-folder" label="Project Information" v-model="content" color="green" />
+            </v-form>               
+          </v-card-text>
+          <v-card-actions>
+              <v-btn flat class="teal lighten-2 mx-0 mt-3" @click.stop="addProject">Add Project</v-btn>
+          </v-card-actions>        
       </v-card>
     </v-dialog>    
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            title: '',
+            content: ''
+        }
+    },
+    methods:{
+        addProject(){
+            
+        }
+    }
 }
 </script>
 
