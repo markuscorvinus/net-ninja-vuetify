4a86e3016f58660543ff858a024b870c96754431
Corvinus <markus.corvinus0827@gmail.com>
2020-06-30 01:13:22 +0800

    #25-Forms-Datepicker(part2)

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -13,11 +13,30 @@
             <v-form class="py-3">
                 <v-text-field prepend-icon="mdi-pencil" label="Title" v-model="title" color="green" />
                 <v-textarea prepend-icon="mdi-folder" label="Project Information" v-model="content" color="green" />
+                <v-menu
+                  :close-on-content-click="false"
+                  transition="scale-transition"
+                  offset-y
+                  max-width="290px"
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      :value="date"
+                      label="Due Date"
+                      hint="MM/DD/YYYY format"
+                      persistent-hint
+                      prepend-icon="mdi-calendar"
+                      @blur="date = parseDate(dateFormatted)"
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker v-model="date" no-title @input="menu2 = false"></v-date-picker>
+                </v-menu>
+                <v-btn flat class="teal lighten-2 mx-0 mt-3" @click.stop="addProject">Add Project</v-btn>
             </v-form>               
-          </v-card-text>
-          <v-card-actions>
-              <v-btn flat class="teal lighten-2 mx-0 mt-3" @click.stop="addProject">Add Project</v-btn>
-          </v-card-actions>        
+          </v-card-text>          
       </v-card>
     </v-dialog>    
 </template>
@@ -27,12 +46,18 @@ export default {
     data(){
         return{
             title: '',
-            content: ''
+            content: '',
+            date: ''
+        }
+    },
+    computed: {
+        computedDateFormatted(){
+            return this.date;
         }
     },
     methods:{
         addProject(){
-            
+
         }
     }
 }
